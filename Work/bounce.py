a511d1a5d80d040296575002920bc30ac39d0893
Zharif <zharifabdulhadi@Zharifs-MacBook-Pro.local>
2026-07-29 09:45:13 +0800

    Updated exercise bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -7,7 +7,7 @@ height_loss_factor = 3/5
 height_from_ground = initial_height * height_loss_factor
 num_bounces = 0
 
-while num_bounces < 10:
+while num_bounces <= 10:
     print(num_bounces + 1, height_from_ground)
     height_from_ground *= height_loss_factor
     num_bounces += 1
